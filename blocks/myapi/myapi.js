9d43550a9dd853ad5e6b00d865ed8345e3db358f
riteshmittal <riteshmittal.virgo@gmail.com>
2024-07-17 19:35:30 +0530

    commit

--- blocks/myapi/myapi.js
+++ blocks/myapi/myapi.js
@@ -1,29 +1,25 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
-const a = "";
-const title = fetch("https://jsonplaceholder.typicode.com/todos/1")
-  .then((response) => response.json())
-  .then((data) => {
-    return data.title;
-  });
-
-
-const printTitle = async () => {
-   a = await title;
-  console.log(a);
-};
-
 
 export default function decorate(block) {
   /* change to ul, li */
   const div = document.createElement('div');
 
-printTitle();
+const title = printTitle();
 
-	alert("printTitle"+a);  
-  block.append(a);
+	alert("title"+title);  
+  block.append(title);
 }
 
 
+async function printTitle() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  
+  response.ok;     // => false
+  response.status; // => 404
+
+  const text = await response.title();
+  return text;
+}
 
 
 
