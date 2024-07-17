ff4f8d38b064940a17f0b08ff241bf5723843d52
riteshmittal <riteshmittal.virgo@gmail.com>
2024-07-17 20:02:08 +0530

    commit

--- blocks/myapi/myapi.js
+++ blocks/myapi/myapi.js
@@ -19,7 +19,7 @@ async function printTitle() {
   const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
 
 
-  const text = await response.json();
+  const text =  response.json();
   
   $('#myapi-class').add(text);
   return text;
